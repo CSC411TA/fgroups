a1229fc9a01c6bdecfe160edcba7fa4c3001e36b
Noah Daniels <ndaniels@mac.com>
2020-12-14 21:03:29 -0500

    Fixed string borrowing issue

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,32 @@
+use std::io::BufRead;
 use std::io;
 use std::str;
+use regex::Regex;
+use std::collections::HashMap;
 
 fn main() -> io::Result<()> {
-    let mut input = String::new();
-    io::stdin().read_line(&mut input)?;
-    let parts: Vec<&str> = input.splitn(2, |c|
-    	c == ' ' || c == '\t').collect();
-    println!("Fprint: {}, Name: {}", parts[0], parts[1]);
+	let mut fgroups:HashMap<String, Vec<String>>  = HashMap::new();
+    for line in io::stdin().lock().lines() {
+    	let pattern = Regex::new(r"\s+").unwrap();
+    	let l = line.unwrap();
+	    let parts: Vec<&str> = pattern.splitn(&l, 2).collect::<Vec<&str>>();
+	    let fp = parts[0].to_string();
+	    let name = parts[1].to_string();
+	    match fgroups.get_mut(&fp) {
+	    	Some(names) => {
+	    		names.push((&name).to_string());
+	    	},
+	    	None => {
+	    		fgroups.insert((&fp).to_string(), vec![(&name).to_string()]);
+	    	}
+	    }
+	    let mut sep = "";
+	    for names in fgroups.values() {
+	    	if names.len() > 1 {
+	    		println!("{}{}", sep, names.join("\n"));
+	    	}
+	    	sep = "\n";
+	    }
+    }
     Ok(())
 }
